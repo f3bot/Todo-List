ef73c0267357a185fe44878fc9bd3cf2d0d98dff
f3bot <febot@protonmail.com>
2023-09-09 14:52:56 +0200

    gotta redo this whole shit :D

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import { generateMainContent, generateTaskPropertiesDiv} from "./components/mainContent";
 import { fillProjects } from "./components/miscellaneous/fillWithDoneProjects";
 import { generateSidebar } from "./components/navbar";
-import { renderView } from "./components/rendering/render";
 import { createAllListeners } from "./listeners/createAll";
-import { projectArray } from "./variables/projectArray";
+
 
 generateSidebar();
 generateMainContent();
-createAllListeners();
 fillProjects();
+createAllListeners();
--- src/variables/projectArray.js
+++
@@ -1,5 +0,0 @@
-
-
-let projectArray = [];
-
-export {projectArray}
